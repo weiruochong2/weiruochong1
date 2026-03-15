f4fff6dbcb8ddf9ac8eb04a55c7302354c0eec6b
weiruochong <2454298650@qq.com>
2026-03-15 18:19:43 +0800

    333

---
+++ 3.c
@@ -0,0 +1,13 @@
+#include "temperature.h"
+int main()
+{
+  int *p=malloc(16);
+  p[0]=1;
+  p[1]=2;
+  p[2]=3;
+  p[3]=4;
+  int temp=get_america_temperature(p,4);
+  printf("%d",temp);
+  free(p);
+  p=NULL;
+}
---
+++ temperature.c
@@ -0,0 +1,26 @@
+#include"temperature.h"
+int get_america_temperature(void *ptr,int len)
+{
+    int*arr=(int*)ptr;
+    int sum = 0;
+    for(int i = 0;i<len;i++)
+    {
+         sum += arr[i];
+
+    }
+    return sum;
+
+}
+int get_china_temperature(void *ptr,int len,int temp)
+{
+    int*arr=(int*)ptr;
+    int sum = 0;
+    for(int i = 0;i<len;i++)
+    {
+         sum += arr[i];
+
+    }
+    sum+=temp;
+    return sum;
+
+}
